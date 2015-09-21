28272c925073ece7d0536897892becae1b2e2bc1
Chris Curro <curro@cooper.edu>
2015-09-21 18:30:55 -0400

    find basic functionality

--- find/src/find.c
+++ find/src/find.c
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 int pathCombine(const char * dir, const char * base, char * path) {
 	strcat(path, dir);
@@ -14,13 +19,32 @@ int pathCombine(const char * dir, const char * base, char * path) {
 	return 0;
 }
 
+int statPerms(struct stat fileStat)
+{
+
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+
+    // printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+
+    return 0;
+}
+
+
 int listdir(const char *path) {
 	struct dirent *entry;
 	DIR *dp;
 	char actualpath [PATH_MAX+1];
 
 	realpath(path, actualpath);
-
 
 	dp = opendir(actualpath);
 	if (dp == NULL) {
@@ -29,6 +53,12 @@ int listdir(const char *path) {
 	}
 
 	char newpath [PATH_MAX+1] ;
+	struct stat sb; 
+	struct passwd * userInfo;
+	struct group  * grpInfo;
+	struct tm * tmInfo;
+	char * tmStr;
+	// char * tmPtr = &tmStr[0];
 
 	while((entry = readdir(dp))) {
 		newpath[0] = '\0';
@@ -37,7 +67,25 @@ int listdir(const char *path) {
 
 		pathCombine(actualpath,name,newpath);
 
+		stat(newpath,&sb);
+
 		if (!(strcmp(name,"..") == 0) && !(strcmp(name,".") == 0)) {
+			printf("%d/%d %d ",sb.st_dev,sb.st_ino,sb.st_nlink);
+			statPerms(sb);
+			printf(" ");
+			userInfo = getpwuid(sb.st_uid);
+			printf(userInfo->pw_name);
+			printf(" ");
+			grpInfo = getgrgid(sb.st_gid);
+			printf(grpInfo->gr_name);
+			printf(" ");
+			printf("%d",sb.st_size);
+			printf(" ");
+			tmInfo = localtime(&sb.st_mtime);
+			tmStr = asctime(tmInfo);
+			tmStr[24] = 0;
+			printf(tmStr);
+			printf(" ");
 			printf(newpath);
 			printf("\n");
 		}
@@ -54,7 +102,24 @@ int listdir(const char *path) {
 
 int main(int argc, char * argv[]) {
 
-	listdir("../..");
+	int mtm = 0;
 
-	return 0;
+	while ( (c = getopt(argc, argv, "u:m:")) != -1) {
+		switch (c) {
+			case 'u':
+        		sscanf(optarg, "%u", &bufferSize);
+        		break;
+        	case 'm':
+        		sscanf(optarg, "%d", &bufferSize);
+        		break;
+        	case '?':
+        		break;
+        	default:
+        		printf ("?? getopt returned character code 0%o ??\n", c);
+        	}
+
+        	listdir("../..");
+
+        	return 0;
+        }
 }

8a25a0666d2c235f89b345d90bcb14127b50a106
Chris Curro <curro@cooper.edu>
2015-10-13 08:46:20 -0400

    fix bug on large files, some cleanup

--- castrato/src/castrato.c
+++ castrato/src/castrato.c
@@ -1,4 +1,4 @@
-// For assignment titled "Fixing your cat"
+// castrato.c -- For assignment titled "Fixing your cat"
 #define _GNU_SOURCE 
 #include <unistd.h>
 #include <fcntl.h>
@@ -30,13 +30,8 @@ void errReport(int returnVal, char * errStr) {
     condChk(returnVal == -1, str);
 }
 
-void cpipe( int p[2]) {
-    for (int i = 0; i < 2; i++)
-        errReport(close(p[i]),"Error on pipe close: ");
-}
-
 int main(int argc, char * argv[]) {
-    condChk(signal(SIGINT, byteReport) == SIG_ERR, "An error occured while setting a signal handler");
+    condChk(signal(SIGINT, byteReport) == SIG_ERR, "An error occured while setting a signal handler\n");
     condChk(argc < 3, "Too few input arguments.\n ex: castrato pattern infile1 [...infile2...]\n");
 
     uint bufferSize = 1024;
@@ -50,15 +45,11 @@ int main(int argc, char * argv[]) {
         errReport(pipe2(more_pipe,O_CLOEXEC), "Error on pipe open: ");
         errReport(pipe2(grep_pipe,O_CLOEXEC), "Error on pipe open: ");
 
-        int fd = open(argv[findex], O_RDONLY); 
-        errReport(fd, argv[findex]);
-        totalFiles++;
-
         more_pid = fork();
         errReport(more_pid,"Failed to open more: ");
 
         if (more_pid == 0) {
-            errReport(dup2(more_pipe[0],STDIN_FILENO),"Error on dup2 of more_piep to stdin: ");
+            errReport(dup2(more_pipe[0],STDIN_FILENO),"Error on dup2 of more_pipe to stdin: ");
             errReport(execvp("more",NULL),"Failed to open more: ");
         }
 
@@ -68,11 +59,19 @@ int main(int argc, char * argv[]) {
         if (grep_pid == 0) {
             errReport(dup2(grep_pipe[0],STDIN_FILENO),"Error on dup2 of grep_pipe to stdin: ");
             errReport(dup2(more_pipe[1],STDOUT_FILENO),"Error on dup2 of more_pipe to stdout: ");;
-            errReport(execlp("grep","grep",argv[1], (char *) NULL),"Failed to open grep: ");
+            errReport(execlp("grep","grep",argv[1], NULL),"Failed to open grep: ");
         }
+
+        int fd = open(argv[findex], O_RDONLY); 
+        errReport(fd, argv[findex]);
+        totalFiles++;
 
         int nBytesRead = 1;
         unsigned char buffer[bufferSize];
+
+        errReport(close(grep_pipe[0]), "Error on close of read end of grep pipe in parent: ");
+        errReport(close(more_pipe[0]), "Error on close of read end of more pipe in parent: ");
+        errReport(close(more_pipe[1]), "Error on close of write end of more pipe in parent: ");
 
         while (nBytesRead != 0) {
             nBytesRead = read(fd,buffer,bufferSize);
@@ -82,9 +81,8 @@ int main(int argc, char * argv[]) {
         }
 
         errReport(close(fd),"Error on close of input: ");
+        errReport(close(grep_pipe[1]), "Error on close of write end of grep pipe in parent: ");
 
-        cpipe(more_pipe);
-        cpipe(grep_pipe);
         wait(0); // one wait for each child
         wait(0);
     }
